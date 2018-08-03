444e52adb51c52f2307be13a1d64ab999679dfaf
Eprastyo <eekoprastyoo@gmail.com>
2018-08-03 08:51:23 +0700

    Update Database

--- magang.sql
+++ magang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2018 at 04:00 AM
+-- Generation Time: Aug 03, 2018 at 03:49 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `t_data_user` (
-  `id_user` varchar(20) NOT NULL,
+  `id_user` int(5) NOT NULL,
   `nama` varchar(20) NOT NULL,
   `password` varchar(15) NOT NULL,
   `level` varchar(20) NOT NULL
@@ -40,9 +40,11 @@ CREATE TABLE `t_data_user` (
 --
 
 INSERT INTO `t_data_user` (`id_user`, `nama`, `password`, `level`) VALUES
-('0001ST', 'Ahmad', '123', 'staff'),
-('0002MN', 'Rudi', '123', 'manager'),
-('0002ST', 'Soni', '321', 'staff');
+(1, 'Rudi', '123', 'manager'),
+(10, 'Dodi', '123', 'staff'),
+(11, 'Ardi', '123', 'staff'),
+(12, 'admin', 'admin', 'manager'),
+(14, 'staff', 'staff', 'staff');
 
 -- --------------------------------------------------------
 
@@ -52,25 +54,29 @@ INSERT INTO `t_data_user` (`id_user`, `nama`, `password`, `level`) VALUES
 
 CREATE TABLE `t_data_utama` (
   `no` int(10) NOT NULL,
+  `kode_project` varchar(100) NOT NULL,
   `nama_pic` varchar(20) NOT NULL,
-  `nama_project` varchar(20) NOT NULL,
+  `nama_project` varchar(500) NOT NULL,
   `instansi` varchar(100) NOT NULL,
   `type` varchar(100) NOT NULL,
   `divisi` varchar(100) NOT NULL,
   `esti_pendapatan` int(20) NOT NULL,
   `real_pendapatan` int(20) NOT NULL,
-  `tanggal` date NOT NULL
+  `tanggal` date NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `info` varchar(300) NOT NULL,
+  `no_spk` varchar(100) NOT NULL,
+  `tgl_spk` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `t_data_utama`
 --
 
-INSERT INTO `t_data_utama` (`no`, `nama_pic`, `nama_project`, `instansi`, `type`, `divisi`, `esti_pendapatan`, `real_pendapatan`, `tanggal`) VALUES
-(14, 'Dodi', 'Software', 'AAA', 'Upgrade', 'Infrastruktur', 200000000, 400000000, '2018-07-19'),
-(15, 'Ahmad', 'Infrastruktur', 'Polindra', 'Baru', 'Infrastruktur', 300000000, 300000000, '2018-07-20'),
-(16, 'Toni', 'Internet', 'ASD', 'Baru', 'ISP', 200000000, 500000000, '2018-07-24'),
-(22, 'Aditya', 'Penambahan Bandwith', 'ASD', 'Upgrade', 'ISP', 100000000, 70000000, '2018-07-26');
+INSERT INTO `t_data_utama` (`no`, `kode_project`, `nama_pic`, `nama_project`, `instansi`, `type`, `divisi`, `esti_pendapatan`, `real_pendapatan`, `tanggal`, `status`, `info`, `no_spk`, `tgl_spk`) VALUES
+(36, '123asd', 'Dodi', 'aassd', 'dsa', 'New', 'Software Development ', 987283824, 734892374, '2018-08-02', 'Development', 'Oke bos', '123', '2018-08-02'),
+(37, '826846823jsdgjsj', 'Ardi', 'aksdhasldkas', 'skdhkaskd', 'Upgrade', 'ISP (Internet Service Provider)', 1000000000, 2147483647, '0000-00-00', 'Development', 'Proses', '2268463287', '2018-08-02'),
+(38, 'yuqwetqwue273', 'Ardi', 'hsdkhaskjdh', 'kjhsdkaks', 'Existing', 'Software Development ', 72848726, 78238725, '0000-00-00', 'Development', 'jaksdaskdgaskjd', '47476', '2018-08-02');
 
 -- --------------------------------------------------------
 
@@ -79,9 +85,19 @@ INSERT INTO `t_data_utama` (`no`, `nama_pic`, `nama_project`, `instansi`, `type`
 --
 
 CREATE TABLE `t_department` (
-  `kode_department` varchar(20) NOT NULL,
+  `id_department` int(20) NOT NULL,
   `nama_department` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_department`
+--
+
+INSERT INTO `t_department` (`id_department`, `nama_department`) VALUES
+(2, 'Software Development '),
+(3, 'ISP (Internet Service Provider)'),
+(8, 'Infrastruktur'),
+(9, 'BDC');
 
 -- --------------------------------------------------------
 
@@ -144,7 +160,7 @@ ALTER TABLE `t_data_utama`
 -- Indexes for table `t_department`
 --
 ALTER TABLE `t_department`
-  ADD PRIMARY KEY (`kode_department`);
+  ADD PRIMARY KEY (`id_department`);
 
 --
 -- Indexes for table `t_divisi`
@@ -163,10 +179,22 @@ ALTER TABLE `t_type`
 --
 
 --
+-- AUTO_INCREMENT for table `t_data_user`
+--
+ALTER TABLE `t_data_user`
+  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
 -- AUTO_INCREMENT for table `t_data_utama`
 --
 ALTER TABLE `t_data_utama`
-  MODIFY `no` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `no` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `t_department`
+--
+ALTER TABLE `t_department`
+  MODIFY `id_department` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `t_divisi`
